aa2fca6488dc28c9e2577773b846f30529407986
Wenguo Liu <liuwenguo@gmail.com>
2020-01-31 17:31:43 +0800

    calculated image similarity

--- src/LabReport.py
+++ src/LabReport.py
@@ -21,11 +21,13 @@ class LabReport(object):
         self.studentName = 'Unknown'
         self.filename = filename
         self.keywords = {}
-        self.similarityReport = {}
+        self.similarityText = {}
+        self.similarityImage = []
         tag1 = '实验步骤'
         tag2 = '实验总结'
         self.keywords[tag1] = {}
         self.keywords[tag2] = {}
+        self.imgfileMd5 = set()
         studentInfo = self.getStudentInfo(filename)
         if studentInfo:
             self.studentID, self.studentName = studentInfo
@@ -33,10 +35,13 @@ class LabReport(object):
             if reportData:
                 self.rawtext, self.imgfileMd5 = reportData
                 self.keywords = self.parseReportText(self.rawtext)
+                self.status = 'ok'
             else:
-                print('不能解析文件', self.filename)
+                self.status = '文件损坏或格式不符合，不能正确解析'
+                print(self.filename, self.status)
         else:
-            print("文件名命名不符合规范，不能正确获取学生信息")
+            self.status = '文件名称命名不规范,不能正确获取学生信息'
+            print(self.filename, self.status)
 
     def getStudentInfo(self, filename):
         matchObj = re.match(r'(\d{12})(.*)\.docx', os.path.basename(filename))
@@ -44,7 +49,6 @@ class LabReport(object):
             id = matchObj.group(1)
             name = matchObj.group(2)
             name = re.sub('-|实验.*|－|（.*）|_|\s|\(.*\)|\+', '', name)
-            print(id, name)
             return id, name
         else:
             print('文件格式不符合命名规则，无法自动识别')
@@ -52,7 +56,6 @@ class LabReport(object):
 
     def getReportData(self, filename):
         imgdir = os.path.join(os.path.dirname(filename), 'tmp')
-        print(filename)
         if not os.path.exists(imgdir):
             os.mkdir(imgdir)
         try:
@@ -61,7 +64,7 @@ class LabReport(object):
             print('文件格式不正确，无法获取数据', filename)
             return None
         else:
-            imgfilesMd5 = [(os.path.basename(f), md5(f)) for f in glob.glob(imgdir + "/*", recursive=True)]
+            imgfilesMd5 = {(os.path.basename(f), md5(f)) for f in glob.glob(imgdir + "/*", recursive=True)}
             text = re.sub('[\n]+', '\n', text)  # remove all empty lines
             shutil.rmtree(imgdir)
 
@@ -70,14 +73,14 @@ class LabReport(object):
     def parseReportText(self, text):
         tag1 = '实验步骤'
         tag2 = '实验总结'
-        parsedText = {}
+        self.parsedText = {}
         keywords = {}
-        parsedText[tag1] = text[text.find(tag1):text.find(tag2)]
-        parsedText[tag2] = text[text.find(tag2):]
-        parsedText[tag1] = re.sub(tag1, '', parsedText[tag1])
-        parsedText[tag2] = re.sub(tag2, '', parsedText[tag2])
-        keywords[tag1] = self.getKeywordFromText(parsedText[tag1])
-        keywords[tag2] = self.getKeywordFromText(parsedText[tag2])
+        self.parsedText[tag1] = text[text.find(tag1):text.find(tag2)]
+        self.parsedText[tag2] = text[text.find(tag2):]
+        self.parsedText[tag1] = re.sub(tag1, '', self.parsedText[tag1])
+        self.parsedText[tag2] = re.sub(tag2, '', self.parsedText[tag2])
+        keywords[tag1] = self.getKeywordFromText(self.parsedText[tag1])
+        keywords[tag2] = self.getKeywordFromText(self.parsedText[tag2])
         return keywords
 
     def getKeywordFromText(self, text):
@@ -90,8 +93,23 @@ class LabReport(object):
         else:
             return ["无"]
 
-    def appendSimilarityReport(self, tag, peer):
-        if not self.similarityReport.keys().__contains__(tag):
-            self.similarityReport[tag] = []
+    def appendSimilarityText(self, tag, peer):
+        if not self.similarityText.keys().__contains__(tag):
+            self.similarityText[tag] = []
 
-        self.similarityReport[tag].append(peer)
+        self.similarityText[tag].append(peer)
+
+    def appendSimilarityImage(self, peer):
+        self.similarityImage.append(peer)
+
+    def print(self):
+        print(self.studentID, self.studentName, ":")
+        for keyword in self.keywords:
+            print("\t key words:", len(self.keywords[keyword]), keyword.title(), ":", self.keywords[keyword])
+        # for word in self.parsedText:
+        #     print("\t total words:", len(self.parsedText[word]), word.title())
+        print("\t total images:", len(self.imgfileMd5))
+        if self.similarityImage:
+            print("\t same pictures in other reports:", max([img['similarity'] for img in self.similarityImage]))
+        for tag in self.similarityText:
+            print("\t same text", tag, "in other reports:", max([txt['similarity'] for txt in self.similarityText[tag]]))
--- src/parse.py
+++ src/parse.py
@@ -9,10 +9,9 @@ from gensim import corpora, models, similarities
 datafile = '/Users/wliu/PycharmProjects/HWAutoChecker/data/云平台管理技术/实验1'
 
 
-def calculateSimilarity(reports, tag):
+def calculateSimilarity(reports, tag, threshold=0.8):
     corpus = []
     for report in reports:
-        print(report.studentName, tag, report.keywords[tag])
         corpus.append(report.keywords[tag])
 
     # 以下使用doc2bow制作语料库 使用TF-IDF模型对语料库建模
@@ -28,24 +27,49 @@ def calculateSimilarity(reports, tag):
         test_tfidf = tfidf[corpus]
         result = similar_matrix[test_tfidf]
         for value, _report in zip(result, reports):
-            if value > 0.8 and report != _report:
-                report.appendSimilarityReport(tag, {'studentID': _report.studentID, 'studentName': _report.studentName,
+            if value > threshold and report != _report:
+                report.appendSimilarityText(tag, {'studentID': _report.studentID, 'studentName': _report.studentName,
                                                     'similarity': value})
                 # print(report.studentID, report.studentName, "-- 相似度 {:.2f}".format(value), _report.studentID, _report.studentName)
 
+def calculateImageSimilarity(reports, threshold=0.2):
+    for report in reports:
+        imgfileMd5 = report.imgfileMd5
+        imgSimilarity = [len(imgfileMd5.intersection(_report.imgfileMd5))/len(report.imgfileMd5) if len(report.imgfileMd5)>0 and report!=_report else 0 for _report in reports ]
+        #get similarity info
+        for value, _report in zip(imgSimilarity, reports):
+            if value > threshold and report != _report:
+                report.appendSimilarityImage( {'studentID': _report.studentID, 'studentName': _report.studentName,
+                                                  'similarity': value})
 
 def traversal(path):
     files = [f for f in glob.glob(path + "**/*.docx", recursive=True)]
     reports = []
+    reportsFailed=[]
     for f in files:
-        reports.append(LabReport.LabReport(f))
+        report = LabReport.LabReport(f)
+        if report.status == 'ok':
+            reports.append(report)
+        else:
+            reportsFailed.append(report)
     calculateSimilarity(reports, "实验总结")
     # calculateSimilarity(reports, "实验步骤")
+    calculateImageSimilarity(reports)
+
+    print("total number of files:", len(files))
+    print("total number of correctly parsed records:", len(reports))
+    print("total number of failed to parse records:", len(reportsFailed))
+    for report in reportsFailed:
+        print(report.filename, report.status)
 
     for report in reports:
-        if report.similarityReport:
-            print(report.studentID, '-', report.studentName, ':')
-            print(report.similarityReport)
+        report.print()
+    #     if len(report.similarityText):
+    #         print(report.studentID, '-', report.studentName, ':')
+    #         print(report.similarityText)
+    #     if len(report.similarityImage):
+    #         print(report.studentID, '-', report.studentName, ':')
+    #         print(report.similarityImage)
 
 
 traversal(datafile)
